65ccb46884ffbd2c8bda34737758ba464867277c
LeKe <95280282+goflutterjava@users.noreply.github.com>
2022-06-14 15:10:37 +0800

    optimize pom file (#41)

---
+++ action-kafka/src/main/java/com/github/shoothzj/pf/consumer/action/kafka/KafkaAction.java
@@ -0,0 +1,42 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.github.shoothzj.pf.consumer.action.kafka;
+
+import com.github.shoothzj.pf.consumer.action.AbstractAction;
+import com.github.shoothzj.pf.consumer.action.module.ActionMsg;
+
+import java.util.List;
+
+public class KafkaAction extends AbstractAction {
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void handleBatchMsg(List<ActionMsg> msgList) {
+
+    }
+
+    @Override
+    public void handleMsg(ActionMsg msg) {
+
+    }
+}
