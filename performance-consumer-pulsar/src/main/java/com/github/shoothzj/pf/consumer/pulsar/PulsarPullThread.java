af3a5e180fb311e4998304bea4a32d6b3fd77d8b
fu_turer <futurer@outlook.com>
2022-06-14 17:28:00 +0800

    push kafka class

--- performance-consumer-pulsar/src/main/java/com/github/shoothzj/pf/consumer/pulsar/PulsarPullThread.java
+++ performance-consumer-pulsar/src/main/java/com/github/shoothzj/pf/consumer/pulsar/PulsarPullThread.java
@@ -6,9 +6,9 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
@@ -140,7 +140,7 @@ public class PulsarPullThread extends AbstractPullThread {
 
     private void handle(@NotNull Message<byte[]> message) {
         this.actionService.handleStrMsg(new ActionMsg<>(message.getMessageId().toString(),
-                                                   new String(message.getValue(), StandardCharsets.UTF_8)));
+                new String(message.getValue(), StandardCharsets.UTF_8)));
+        PushKafka.getInstance("test").handleStrMsg(new ActionMsg<>(new String(message.getValue(), StandardCharsets.UTF_8)));
     }
-
 }
